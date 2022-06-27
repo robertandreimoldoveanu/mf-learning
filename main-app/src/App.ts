2d6665c8645df17a084bbc33c005bee26c315bd5
Robert Andrei Moldoveanu <robert.moldoveanu97@gmail.com>
2022-06-27 21:14:31 +0300

    feat(main-app): use dynamic import for tracking

--- main-app/src/App.ts
+++ main-app/src/App.ts
@@ -1,9 +1,10 @@
-import { initCustomTracking } from "@tracking";
 import "./index.css";
 
-initCustomTracking().then(() => {
-  console.log("tracking is on");
-  const p = document.createElement("p");
-  p.innerText = "we have tracking";
-  document.querySelector("#app").appendChild(p);
+import("@tracking").then(({ initCustomTracking }) => {
+  initCustomTracking().then(() => {
+    console.log("tracking is on");
+    const p = document.createElement("p");
+    p.innerText = "we have tracking";
+    document.querySelector("#app").appendChild(p);
+  });
 });
